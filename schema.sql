7831fe50d95e74039915ff530d82f5ac92146717
Cnicolebrown <103790879+Cnicolebrown@users.noreply.github.com>
2022-05-23 19:17:33 -0500

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,34 @@
+-- creating tables for ph_employeeDB
+create table departments(
+dept_no varchar(4) not null,
+dept_name varchar(40) not null,
+primary key (dept_no),
+unique (dept_name)
+);
+create table employees(
+emp_no int not null,
+birth_date Date not null,
+first_name varchar not null,
+last_name varchar not null,
+gender varchar not null,
+hire_date date not null,
+primary key (emp_no)
+);
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+select * from departments
